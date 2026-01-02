0d9a49bc77b12ea7b64757e38c309bb10508c7a9
Roman Golovko <rgolovkl6@gmail.com>
2026-01-02 12:34:07 +0300

    feat: update middleware

--- internal/transport/rest/v1/middleware.go
+++ internal/transport/rest/v1/middleware.go
@@ -1,15 +1,28 @@
 package v1
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/koan6gi/notifier/pkg/logger"
 )
+
+type Updater interface {
+	Update(ctx context.Context)
+}
 
 func LoggingMiddleware(next http.Handler, lg *logger.Logger) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		req := r.WithContext(logger.WithContext(r.Context(), lg))
 
 		next.ServeHTTP(w, req)
+	})
+}
+
+func UpdateMiddleware(next http.Handler, u Updater) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		u.Update(r.Context())
+
+		next.ServeHTTP(w, r)
 	})
 }
